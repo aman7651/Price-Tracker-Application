21b1507d48868c37ca6d2a5e2b49898869e3ee57
aman7651 <67210218+aman7651@users.noreply.github.com>
2020-06-26 14:39:20 +0530

    Update forms.py

--- tracker/forms.py
+++ tracker/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class AddNewItemForm(forms.Form):
     url = forms.CharField(max_length=600)
     requested_price = forms.IntegerField()
-    email =form.Charfield(max_length =400)
+    email =form.CharField(max_length =400)
