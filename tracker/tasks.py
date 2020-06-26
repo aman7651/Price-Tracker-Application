d1dffdc5f0fa57f41e7ac48cdd6e170fe88d6e29
aman7651 <67210218+aman7651@users.noreply.github.com>
2020-06-26 17:16:18 +0530

    Update tasks.py

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from .models import Item
 from tracker.views import crawl_data
 
+
 @shared_task
 def track_for_discount():
     items = Item.objects.all()
@@ -13,7 +14,25 @@ def track_for_discount():
             item_discount = Item.objects.get(id=item.id)
             item_discount.discount_price = f'DISCOUNT! The price is {data["last_price"]}'
             item_discount.save()
-            
+            send_mail()
+@shared_task
+                  
+def send_mail():
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login('aman765180@gmail.com', 'Neesu@123')
+        subject = "Price of your Flipkart item has fallen down"
+        body = "Hey  \User The price of Flipkart product has reached at your requested price     
+           : "
+        msg = f"Subject: {subject} \n\n {body} "
+        server.sendmail(
+            'aman765180@gmail.com',
+            'Abhishek7071631646@gmail.com', msg
+        )
+        ,#print("HEY Rahul, EMAIL HAS BEEN SENT SUCCESSFULLY.")
+        server.quit()
         
 @shared_task
 def track_for_not_discount():
